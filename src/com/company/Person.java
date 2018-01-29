b0859c6ae6c8376dafe9954b7cad4b9f31d1deb8
Klim Starykau <klim_starykau@epam.com>
2018-01-29 17:41:27 +0300

    Task 6 rev1.

---
+++ src/com/company/Person.java
@@ -0,0 +1,39 @@
+package com.company;
+
+/**
+ * Created by Klim_Starykau on 1/22/2018.
+ */
+public class Person {
+
+    private int userId;
+    private String clientName;
+    private String userPrivilege = "Base privilege";
+
+
+    protected Person(int userId, String clientName, String userPrivilege) {
+        this.userId = userId;
+        this.clientName = clientName;
+        this.userPrivilege = userPrivilege;
+    }
+
+
+    public Person() {
+    }
+
+    ;
+
+    public void AssingToClient(String newClientName) {
+        this.clientName = newClientName;
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "userId=" + userId +
+                ", clientName='" + clientName + '\'' +
+                ", userPrivilege='" + userPrivilege + '\'' +
+                '}';
+    }
+
+}
