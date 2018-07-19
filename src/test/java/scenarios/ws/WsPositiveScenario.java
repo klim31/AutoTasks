8efeafb943ddadcee6e823533427da88c8e8d2f6
Klim Starykau <klim_starykau@epam.com>
2018-07-19 17:12:51 +0300

    Unnecessary field dropped

--- src/test/java/scenarios/ws/WsPositiveScenario.java
+++ src/test/java/scenarios/ws/WsPositiveScenario.java
@@ -10,20 +10,19 @@ public class WsPositiveScenario {
 
     WsService wsService = new WsService();
     private static final int status = 200;
-    private String capitalName;
 
     @BeforeClass
     @Parameters({"URL", "countryCode", "stateCode"})
     public void sendRequest(String URL, String countryCode, String stateCode) {
         System.out.println("\nPositive case");
         wsService.sendRequest(URL, countryCode, stateCode);
-        System.out.println(wsService.getStatusCode()); // TODO: 7/18/2018 just for debug
+        System.out.println(wsService.getStatusCode());
     }
 
 
     @Test(description = "Verify the response")
     public void verifyResponse() {
-        capitalName = wsService.getResponseParameterValue(wsService.getResponse());
+        String capitalName = wsService.getResponseParameterValue(wsService.getResponse());
         System.out.println(capitalName);
         Assert.assertTrue((wsService.getStatusCode() == status) && (capitalName != null));
     }
